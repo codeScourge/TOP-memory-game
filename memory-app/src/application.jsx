fc51935001b5266b192bd4302256bdb69296debc
codeScourge <code.scourge@gmail.com>
2023-07-25 12:57:43 +0300

    added levels and navbar

--- memory-app/src/application.jsx
+++ memory-app/src/application.jsx
@@ -3,18 +3,24 @@ import ImageCard from "./components/memory-game/imageCard";
 import ScoreBoard from "./components/memory-game/scoreBoard";
 import arrayShuffle from 'array-shuffle';
 
-const nImages = 6;  // for aesthetically pleasing layouts should be divisible by 3
-
-let numberArray = [];
-for (let i = 1; i <= nImages; i++) {
-    numberArray.push(i);
-}
-
+const maxImages = 6;    // current amount of uploaded monster images in public folder
 
 export default function Application() {
     const [score, setScore] = useState(0);
     const [highScore, setHighScore] = useState(0);
     const [rounds, setRounds] = useState(0);
+    const [nImages, setNImages] = useState(3);
+
+    function nextDifficulty() {
+        resetScore();
+        if (nImages >= maxImages) {
+            setNImages(3);
+            console.log("You just one the whole game!");
+        } else {
+            console.log("Next round!");
+            setNImages(nImages + 3);
+        }
+    }
 
     function incrementScore() {
         setScore(score + 1);
@@ -25,21 +31,33 @@ export default function Application() {
         setRounds(rounds + 1);
     };
 
+    function resetGame() {
+        resetScore();
+        setNImages(3);
+    }
+
     useEffect(() => {
         if (score > highScore) {
             setHighScore(score);
-            if (score >= nImages) {
-                console.log("You Won!");
-                resetScore();
-            }
+        }
+
+        if (score >= nImages) {
+            nextDifficulty();
         }
     }, [score]);
+
+
 
     // Q: why does this work? Wont the imageArray be a new one with new states every time?
 
     // A: It is because the node is already existing, which means it will not remount and set default states, but rerender them with the new
     // parameters. This mean shuffling it wont change the position of the elements, but their props, which will break everything. This means
     // we should probably work with a grids css properties
+
+    let numberArray = [];
+    for (let i = 1; i <= nImages; i++) {
+        numberArray.push(i);
+    }
 
     const imageArray = arrayShuffle(numberArray).map((number, index) => {return <ImageCard incrementScore={incrementScore} resetScore={resetScore} rounds={rounds} index={number} keyId={index + 1}></ImageCard>})
 
@@ -54,6 +72,6 @@ export default function Application() {
                     {imageArray}
                 </div>
 
-                <button onClick={resetScore} className="wrapper__reset"> restart game </button>
+                <button onClick={resetGame} className="wrapper__reset"> restart game </button>
             </div>
 }
--- memory-app/src/components/memory-game/scoreBoard.jsx
+++ memory-app/src/components/memory-game/scoreBoard.jsx
@@ -2,9 +2,14 @@ import React from "react";
 
 export default function ScoreBoard({currentScore, highScore, rounds}) {
     return  <div className="scoreboard">
-                <h3 className="scoreboard__title">SCOREBOARD</h3>
-                <h5 className="scoreboard__score">current score: {currentScore}</h5>
-                <h5 className="scoreboard__score">highest score: {highScore}</h5>
-                <h5 className="scoreboard__score">current round: {rounds}</h5>
+                <h1 className="scoreboard__logo">
+                    MySingingMonsters
+                </h1>
+                <div className="scoreboard__wrapper">
+                    <h3 className="scoreboard__wrapper__title">SCOREBOARD</h3>
+                    <h5 className="scoreboard__wrapper__score">current score: {currentScore}</h5>
+                    <h5 className="scoreboard__wrapper__score">highest score: {highScore}</h5>
+                    <h5 className="scoreboard__wrapper__score">current round: {rounds}</h5>
+                </div>
             </div>
 }
